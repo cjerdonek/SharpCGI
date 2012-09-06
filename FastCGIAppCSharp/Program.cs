1ae6c9efcb1705c91d533344f840bc4bb0f92887
Chris Jerdonek <chris.jerdonek@gmail.com>
2012-09-05 18:32:52 -0700

    Configure trace and error logging from C#

--- FastCGIAppCSharp/Program.cs
+++ FastCGIAppCSharp/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Globalization;
 using System.Text;
 
+using Core = Microsoft.FSharp.Core;
+
 using log4net;
 using log4net.Config;
 
@@ -14,9 +16,12 @@ namespace FastCGIApp
     class Program
     {
         private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+        private static readonly ILog flog = LogManager.GetLogger("FastCGI");
 
         static void HandleRequest(Request request, Response response)
         {
+            log.Debug("Handling request...");
+
             // receive HTTP content
             byte[] content = request.Stdin.GetContents();
 
@@ -48,16 +53,39 @@ namespace FastCGIApp
                 );
         }
 
+        // Crazy code to convert from C# delegates to F# functions.
+        // For example, see: http://blogs.msdn.com/b/jaredpar/archive/2010/07/27/converting-system-func-lt-t1-tn-gt-to-fsharpfunc-lt-t-tresult-gt.aspx
+
+        delegate Core.Unit OnStringDelegate(string s);
+
+        static Converter<string, Core.Unit> MakeConverter(Action<string> onString)
+        {
+            OnStringDelegate del = (s) => { onString(s); return null; };
+            return new Converter<string, Core.Unit>(s => del(s));
+        }
+
+        static Core.FSharpFunc<string, Core.Unit> MakeFSharp(Action<string> onString)
+        {
+            Converter<string, Core.Unit> converter = MakeConverter(onString);
+            return Core.FSharpFunc<string, Core.Unit>.FromConverter(converter);
+        }
+
         static void Main(string[] args)
         {
             XmlConfigurator.Configure();
 
-            log.Info("Starting: FastCGIApp");
+            log.Info("Starting: Main");
 
             Options config = new Options();
             config.Bind = BindMode.CreateSocket;
             config.EndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000);
-            config.OnError = Console.WriteLine;
+
+            config.OnError = (s) => { flog.Error(s); };
+            config.OnTrace = (s) => { flog.Debug(s); };
+
+            //config.ErrorLogger = MakeFSharp(s => flog.Error(s));
+            //config.TraceLogger = MakeFSharp(s => flog.Debug(s));
+            
             Server.Start(HandleRequest, config);
         }
     }
